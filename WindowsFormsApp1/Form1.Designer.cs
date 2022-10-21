fabcf6aad3b9ac35712a4e3d40b86ab57c166946
student <student@s004-pc05>
2022-10-21 15:59:14 +0200

    zmiany na dom

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -32,13 +32,14 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.userBox = new System.Windows.Forms.TextBox();
+            this.imgPassBox = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.randPicBox = new System.Windows.Forms.PictureBox();
             this.randPic = new System.Windows.Forms.LinkLabel();
-            this.button1 = new System.Windows.Forms.Button();
+            this.LogIn = new System.Windows.Forms.Button();
+            this.Error = new System.Windows.Forms.Label();
+            this.passwordBox = new System.Windows.Forms.MaskedTextBox();
+            this.randPicBox = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.randPicBox)).BeginInit();
             this.SuspendLayout();
             // 
@@ -72,44 +73,28 @@
             this.label3.TabIndex = 2;
             this.label3.Text = "Weryfikacja";
             // 
-            // textBox1
+            // userBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(223, 38);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(144, 20);
-            this.textBox1.TabIndex = 3;
+            this.userBox.Location = new System.Drawing.Point(223, 38);
+            this.userBox.Name = "userBox";
+            this.userBox.Size = new System.Drawing.Size(144, 20);
+            this.userBox.TabIndex = 3;
             // 
-            // textBox2
+            // imgPassBox
             // 
-            this.textBox2.Location = new System.Drawing.Point(223, 87);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(144, 20);
-            this.textBox2.TabIndex = 4;
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(223, 236);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(144, 20);
-            this.textBox3.TabIndex = 5;
+            this.imgPassBox.Location = new System.Drawing.Point(223, 236);
+            this.imgPassBox.Name = "imgPassBox";
+            this.imgPassBox.Size = new System.Drawing.Size(144, 20);
+            this.imgPassBox.TabIndex = 5;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(114, 191);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(322, 13);
+            this.label4.Size = new System.Drawing.Size(327, 13);
             this.label4.TabIndex = 6;
-            this.label4.Text = "Wpisz znaki, które widzisz na poniżzym obrazku, aby kontynuować";
-            // 
-            // randPicBox
-            // 
-            this.randPicBox.Image = global::WindowsFormsApp1.Properties.Resources._1;
-            this.randPicBox.Location = new System.Drawing.Point(117, 224);
-            this.randPicBox.Name = "randPicBox";
-            this.randPicBox.Size = new System.Drawing.Size(100, 32);
-            this.randPicBox.TabIndex = 7;
-            this.randPicBox.TabStop = false;
+            this.label4.Text = "Wpisz znaki, które widzisz na poniższym obrazku, aby kontynuować";
             // 
             // randPic
             // 
@@ -123,28 +108,56 @@
             this.randPic.Text = "losuj nowy";
             this.randPic.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.randPic_LinkClicked);
             // 
-            // button1
+            // LogIn
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.button1.Location = new System.Drawing.Point(361, 293);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 9;
-            this.button1.Text = "Zaloguj";
-            this.button1.UseVisualStyleBackColor = true;
+            this.LogIn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.LogIn.Location = new System.Drawing.Point(361, 293);
+            this.LogIn.Name = "LogIn";
+            this.LogIn.Size = new System.Drawing.Size(75, 23);
+            this.LogIn.TabIndex = 9;
+            this.LogIn.Text = "Zaloguj";
+            this.LogIn.UseVisualStyleBackColor = true;
+            this.LogIn.Click += new System.EventHandler(this.LogIn_Click);
+            // 
+            // Error
+            // 
+            this.Error.AutoSize = true;
+            this.Error.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Error.Location = new System.Drawing.Point(87, 132);
+            this.Error.Name = "Error";
+            this.Error.Size = new System.Drawing.Size(0, 13);
+            this.Error.TabIndex = 10;
+            // 
+            // passwordBox
+            // 
+            this.passwordBox.Location = new System.Drawing.Point(221, 87);
+            this.passwordBox.Name = "passwordBox";
+            this.passwordBox.PasswordChar = '*';
+            this.passwordBox.Size = new System.Drawing.Size(146, 20);
+            this.passwordBox.TabIndex = 11;
+            // 
+            // randPicBox
+            // 
+            this.randPicBox.Image = global::WindowsFormsApp1.Properties.Resources._1;
+            this.randPicBox.Location = new System.Drawing.Point(117, 224);
+            this.randPicBox.Name = "randPicBox";
+            this.randPicBox.Size = new System.Drawing.Size(100, 32);
+            this.randPicBox.TabIndex = 7;
+            this.randPicBox.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.passwordBox);
+            this.Controls.Add(this.Error);
+            this.Controls.Add(this.LogIn);
             this.Controls.Add(this.randPic);
             this.Controls.Add(this.randPicBox);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.imgPassBox);
+            this.Controls.Add(this.userBox);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -162,13 +175,14 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox userBox;
+        private System.Windows.Forms.TextBox imgPassBox;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.PictureBox randPicBox;
         private System.Windows.Forms.LinkLabel randPic;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button LogIn;
+        private System.Windows.Forms.Label Error;
+        private System.Windows.Forms.MaskedTextBox passwordBox;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -8,20 +8,26 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using WindowsFormsApp1.Properties;
+using System.Threading;
 
 namespace WindowsFormsApp1
+
 {
     public partial class Form1 : Form
     {
+        Thread th;
         public Form1()
         {
             InitializeComponent();
+
         }
 
         private void randPic_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
+
             Random rnd = new Random();
-            int randNum = rnd.Next(1,7);
+            int randNum = rnd.Next(1, 7);
+
             if (randNum == 1)
             {
                 randPicBox.Image = Resources._1;
@@ -50,6 +56,33 @@ namespace WindowsFormsApp1
             {
                 randPicBox.Image = Resources._7;
             }
+        }
+
+        private void LogIn_Click(object sender, EventArgs e)
+        {
+            string user = "admin";
+            string haslo = "Qwerty1@34";
+            string picHaslo;
+            if (randPicBox.Image == Resources._1)
+            {
+                picHaslo = "mxyxw";
+                if (passwordBox.Text == picHaslo)
+                {
+                    this.Close();
+                    th = new Thread(opennewform);
+                    th.SetApartmentState(ApartmentState.STA);
+                    th.Start();
+                }
+                else
+                {
+                    Error.ForeColor = Color.Red;
+                    Error.Text = "Wprowadzono błędną nazwę użytkownika lub/i hasło";
+                }
+            }
+        }
+        private void opennewform(object obj)
+        {
+            Application.Run(new Form2());
         }
     }
 }
