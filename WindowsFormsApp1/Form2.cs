712ebca80feefbd77fb0f2fa21799c6af8584899
student <student@s001-pc05>
2022-11-04 16:47:35 +0100

    nie wiem jakieś tam zmiany

--- WindowsFormsApp1/Form2.cs
+++ WindowsFormsApp1/Form2.cs
@@ -8,37 +8,79 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Net.WebRequestMethods;
+using File = System.IO.File;
 
 namespace WindowsFormsApp1
 {
     public partial class Form2 : Form
     {
+        
+
         public Form2()
         {
             InitializeComponent();
         }
+
+        List<string> students = new List<string>();
         string path = @"C:\Users\student\Desktop\uczen.txt";
+
         private void btnAdd_Click(object sender, EventArgs e)
         {
             if (txtName.Text != "" && txtSurname.Text != "" && txtKlass.Text != "") {
                 string name = txtName.Text;
                 string surname = txtSurname.Text;
                 string klass = txtKlass.Text;
-                using (StreamWriter sw = File.CreateText(path))
-                {
-                    sw.WriteLine(name, " ", surname, " ", klass);
-                }
+                string student = name + " " + surname + " " + klass;
+                students.Add(student);
+                File.WriteAllLines(path, students);
             }
         }
 
         private void btnFind_Click(object sender, EventArgs e)
         {
-            if (File.Exists(path))
+            if (chooseParameter.SelectedIndex == 0)
             {
-                var x = File.ReadAllLines(path).Count();
-                for(int i = 0; i < x; i++)
+                if(chooseOption.SelectedIndex == 0)
                 {
-                    showResult.Items.Add(File.ReadAllLines(path + "/" + i));
+
+                }
+                else if(chooseOption.SelectedIndex == 1)
+                {
+
+                }
+                else if(chooseOption.SelectedIndex == 2)
+                {
+
+                }
+            }
+            else if (chooseParameter.SelectedIndex == 1)
+            {
+                if (chooseOption.SelectedIndex == 0)
+                {
+
+                }
+                else if (chooseOption.SelectedIndex == 1)
+                {
+
+                }
+                else if (chooseOption.SelectedIndex == 2)
+                {
+
+                }
+            }
+            else if (chooseParameter.SelectedIndex == 2)
+            {
+                if (chooseOption.SelectedIndex == 0)
+                {
+
+                }
+                else if (chooseOption.SelectedIndex == 1)
+                {
+
+                }
+                else if (chooseOption.SelectedIndex == 2)
+                {
 
                 }
             }
